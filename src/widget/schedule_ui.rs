eecef1f3893d81d2ee02c1e44fa9b3965d7e2893
Shou Ya <shouya@users.noreply.github.com>
2022-02-01 22:44:19 +0800

    draw current time indicator

--- src/widget/schedule_ui.rs
+++ src/widget/schedule_ui.rs
@@ -1,6 +1,6 @@
 use chrono::{Date, DateTime, Duration, Local};
 use derive_builder::Builder;
-use eframe::egui::{self, Rect, Response, Ui, Vec2};
+use eframe::egui::{self, Color32, Rect, Response, Ui, Vec2};
 
 #[derive(Builder, Debug, PartialEq)]
 pub struct ScheduleUi {
@@ -15,14 +15,19 @@ pub struct ScheduleUi {
   #[builder(default = "80.0")]
   time_marker_margin_width: f32,
   #[builder(default = "60.0")]
-  day_marker_margin_height: f32,
+  day_header_margin_height: f32,
   #[builder(default = "\"%H:%M\"")]
   time_marker_format: &'static str,
   #[builder(default = "\"%F\"")]
-  day_marker_format: &'static str,
+  day_header_format: &'static str,
   #[builder(default = "Local::today()")]
   first_day: Date<Local>,
+  // used to render current time indicator
+  #[builder(default = "Some(Local::now())")]
+  current_time: Option<DateTime<Local>>,
 }
+
+const SECS_PER_DAY: u64 = 24 * 3600;
 
 impl ScheduleUi {
   fn draw_ticks(&self, ui: &mut Ui, rect: Rect) {
@@ -32,7 +37,7 @@ impl ScheduleUi {
     let base_pos = rect.left_top()
       + egui::vec2(
         self.time_marker_margin_width,
-        self.day_marker_margin_height,
+        self.day_header_margin_height,
       );
     let painter = ui.painter_at(rect);
 
@@ -58,10 +63,10 @@ impl ScheduleUi {
 
     // draw the day marks
     for nth_day in 0..self.day_count {
-      let y = -(self.day_marker_margin_height - visuals.clip_rect_margin) / 2.0;
+      let y = -(self.day_header_margin_height - visuals.clip_rect_margin) / 2.0;
       let x = self.day_width * (nth_day as f32 + 0.5);
 
-      let text = self.day_marker_text(nth_day).expect("day out of bound");
+      let text = self.day_header_text(nth_day).expect("day out of bound");
 
       painter.text(
         base_pos + egui::vec2(x, y),
@@ -86,15 +91,39 @@ impl ScheduleUi {
         widget_visuals.text_color(),
       );
     }
+
+    // draw current time indicator
+    if let Some(now) = self.current_time.as_ref() {
+      let beginning_of_day = now.date().and_hms(0, 0, 0);
+      let seconds_past_midnight = (*now - beginning_of_day).num_seconds();
+      let day_progress =
+        (seconds_past_midnight as f32 / SECS_PER_DAY as f32).clamp(0.0, 1.0);
+      let y = day_progress * self.content_height();
+      let x0 = -visuals.clip_rect_margin;
+      let x1 = self.content_width();
+
+      let p0 = base_pos + egui::vec2(x0, y);
+      let p1 = base_pos + egui::vec2(x1, y);
+      let mut indicator_stroke = widget_visuals.bg_stroke;
+      indicator_stroke.color = Color32::RED;
+      painter.line_segment([p0, p1], indicator_stroke);
+    }
   }
 
-  fn day_marker_text(&self, nth_day: usize) -> Option<String> {
+  fn content_height(&self) -> f32 {
+    self.segment_height * self.segment_count as f32
+  }
+  fn content_width(&self) -> f32 {
+    self.day_width * self.day_count as f32
+  }
+
+  fn day_header_text(&self, nth_day: usize) -> Option<String> {
     if nth_day >= self.day_count {
       return None;
     }
 
     let day = self.first_day + Duration::days(nth_day as i64);
-    let formatted_day = day.format(self.day_marker_format);
+    let formatted_day = day.format(self.day_header_format);
 
     Some(format!("{formatted_day}"))
   }
@@ -120,19 +149,25 @@ impl ScheduleUi {
     }
     let day = self.first_day + Duration::days(day as i64);
     let beginning_of_day = day.and_hms(0, 0, 0);
-    let offset = 24 * 3600 / self.segment_count * segment;
+    let offset = SECS_PER_DAY as usize / self.segment_count * segment;
     Some(beginning_of_day + Duration::seconds(offset as i64))
   }
 
-  fn desired_size(&self, _ui: &Ui) -> Vec2 {
+  fn desired_size(&self, ui: &Ui) -> Vec2 {
+    let visuals = ui.style().visuals.clone();
+    let clip_margin = visuals.clip_rect_margin;
+
     // give a bit more vertical space to display the last time mark
     let text_safe_margin = 10.0;
 
     egui::vec2(
-      self.time_marker_margin_width + self.day_width * self.day_count as f32,
-      self.day_marker_margin_height
+      self.time_marker_margin_width
+        + self.day_width * self.day_count as f32
+        + clip_margin,
+      self.day_header_margin_height
         + self.segment_height * self.segment_count as f32
-        + text_safe_margin,
+        + text_safe_margin
+        + clip_margin,
     )
   }
 }
